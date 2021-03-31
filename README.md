56e1775f73d27e3521964830d820b5c8c8b4387b
Parzival <parzival.nieminen@gmail.com>
2021-04-01 00:43:59 +0200

    feat add BlurHashDecoder.kt

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # android-capstone
 Project for: Android Kotlin Developer Nanodegree Program - nd940 - project 5
+
+
+# Libs
+* blurhash image decoder: https://github.com/woltapp/blurhash
+
--- app/src/main/java/ch/theforce/capstone/MainActivity.kt
+++ app/src/main/java/ch/theforce/capstone/MainActivity.kt
@@ -2,10 +2,18 @@ package ch.theforce.capstone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import ch.theforce.capstone.utils.BlurHashDecoder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var photo = findViewById<ImageView>(R.id.photo)
+        val blurHashString = "LSGktJt7W;Rj_Nxaa#WB?cazoLWB"
+        var photoBitmap = BlurHashDecoder.decode(blurHashString, 2160, 3240)
+
+        photo.setImageBitmap(photoBitmap)
+
     }
 }
