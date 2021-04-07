430b2228a427756be26b8dab63e2c9329baea9b7
Parzival <parzival.nieminen@gmail.com>
2021-04-07 21:42:32 +0200

    feat AppBarLayout with scrolling

--- app/src/main/java/ch/theforce/capstone/MainActivity.kt
+++ app/src/main/java/ch/theforce/capstone/MainActivity.kt
@@ -1,9 +1,12 @@
 package ch.theforce.capstone
 
 import android.os.Bundle
-import android.widget.ImageView
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import ch.theforce.capstone.databinding.ActivityMainBinding
 import ch.theforce.capstone.network.UnSplashHttpClient
 import ch.theforce.capstone.utils.BlurHashDecoder
 import com.squareup.picasso.Picasso
@@ -11,12 +14,17 @@ import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var navController: NavController
     private val client: UnSplashHttpClient by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var photo = findViewById<ImageView>(R.id.photo)
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(activityMainBinding.mainToolbar)
 
         lifecycleScope.launch() {
             val randomPhoto = client.getRandomPhoto()
@@ -26,8 +34,13 @@ class MainActivity : AppCompatActivity() {
                     randomPhoto.width / 3,
                     randomPhoto.height / 3
                 )
-            photo.setImageBitmap(photoBitmap)
-            Picasso.get().load(randomPhoto.urls.full).into(photo)
+            activityMainBinding.photo.setImageBitmap(photoBitmap)
+            Picasso.get().load(randomPhoto.urls.full).into(activityMainBinding.photo)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
